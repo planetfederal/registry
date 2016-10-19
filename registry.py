f2957b9b67fea0eec68b0b147c21210679b527be
Ariel Nuñez <ingenieroariel@gmail.com>
2016-10-18 22:59:36 -0500

    Added PYCSW config dict with repository and mapping

--- registry.py
+++ registry.py
@@ -4,18 +4,124 @@ from django.core import management
 from django.conf.urls import url
 from django.http import HttpResponse
 from pycsw import server
+from pycsw.core.repository import Repository
 from distutils.util import strtobool
 from six import StringIO
 
 __version__ = 0.1
 
-DEBUG = strtobool(os.getenv('DEBUG', 'True'))
+DEBUG = strtobool(os.getenv('REGISTRY_DEBUG', 'True'))
 ROOT_URLCONF = 'registry'
 DATABASES = {'default': {}}  # required regardless of actual usage
-SECRET_KEY = os.getenv('SECRET_KEY', 'Please set a SECRET_KEY as an env var.')
+SECRET_KEY = os.getenv('REGISTRY_SECRET_KEY', 'Make sure you create a good secret key.')
 
 if not settings.configured:
     settings.configure(**locals())
+
+PYCSW = {
+        'repository': {
+            'source': 'registry.RegistryRepository',
+            'mappings': 'registry',
+        },
+        'server': {
+            'maxrecords': '100',
+            'pretty_print': 'true',
+            'domaincounts': 'true',
+            'encoding': 'UTF-8',
+            'profiles': 'apiso'
+        },
+        'metadata:main': {
+            'identification_title': 'Registry',
+            'identification_abstract': 'Registry is a CSW catalogue with faceting capabilities via OpenSearch',
+            'identification_keywords': 'registry, pycsw',
+            'identification_keywords_type': 'theme',
+            'identification_fees': 'None',
+            'identification_accessconstraints': 'None',
+            'provider_name': 'Organization Name',
+            'provider_url': '',
+            'contact_name': 'Lastname, Firstname',
+            'contact_position': 'Position Title',
+            'contact_address': 'Mailing Address',
+            'contact_city': 'City',
+            'contact_stateorprovince': 'Administrative Area',
+            'contact_postalcode': 'Zip or Postal Code',
+            'contact_country': 'Country',
+            'contact_phone': '+xx-xxx-xxx-xxxx',
+            'contact_fax': '+xx-xxx-xxx-xxxx',
+            'contact_email': 'Email Address',
+            'contact_url': 'Contact URL',
+            'contact_hours': 'Hours of Service',
+            'contact_instructions': 'During hours of service. Off on weekends.',
+            'contact_role': 'pointOfContact',
+        },
+        'manager': {
+            'transactions': 'true',
+            'allowed_ips': os.getenv('REGISTRY_ALLOWED_IPS', '*'),
+        },
+}
+
+MD_CORE_MODEL = {
+    'typename': 'pycsw:CoreMetadata',
+    'outputschema': 'http://pycsw.org/metadata',
+    'mappings': {
+        'pycsw:Identifier': 'identifier',
+        'pycsw:Typename': 'typename',
+        'pycsw:Schema': 'schema',
+        'pycsw:MdSource': 'mdsource',
+        'pycsw:InsertDate': 'insert_date',
+        'pycsw:XML': 'xml',
+        'pycsw:AnyText': 'anytext',
+        'pycsw:Language': 'language',
+        'pycsw:Title': 'title',
+        'pycsw:Abstract': 'abstract',
+        'pycsw:Keywords': 'keywords',
+        'pycsw:KeywordType': 'keywordstype',
+        'pycsw:Format': 'format',
+        'pycsw:Source': 'source',
+        'pycsw:Date': 'date',
+        'pycsw:Modified': 'date_modified',
+        'pycsw:Type': 'type',
+        'pycsw:BoundingBox': 'wkt_geometry',
+        'pycsw:CRS': 'crs',
+        'pycsw:AlternateTitle': 'title_alternate',
+        'pycsw:RevisionDate': 'date_revision',
+        'pycsw:CreationDate': 'date_creation',
+        'pycsw:PublicationDate': 'date_publication',
+        'pycsw:OrganizationName': 'organization',
+        'pycsw:SecurityConstraints': 'securityconstraints',
+        'pycsw:ParentIdentifier': 'parentidentifier',
+        'pycsw:TopicCategory': 'topicategory',
+        'pycsw:ResourceLanguage': 'resourcelanguage',
+        'pycsw:GeographicDescriptionCode': 'geodescode',
+        'pycsw:Denominator': 'denominator',
+        'pycsw:DistanceValue': 'distancevalue',
+        'pycsw:DistanceUOM': 'distanceuom',
+        'pycsw:TempExtent_begin': 'time_begin',
+        'pycsw:TempExtent_end': 'time_end',
+        'pycsw:ServiceType': 'servicetype',
+        'pycsw:ServiceTypeVersion': 'servicetypeversion',
+        'pycsw:Operation': 'operation',
+        'pycsw:CouplingType': 'couplingtype',
+        'pycsw:OperatesOn': 'operateson',
+        'pycsw:OperatesOnIdentifier': 'operatesonidentifier',
+        'pycsw:OperatesOnName': 'operatesoname',
+        'pycsw:Degree': 'degree',
+        'pycsw:AccessConstraints': 'accessconstraints',
+        'pycsw:OtherConstraints': 'otherconstraints',
+        'pycsw:Classification': 'classification',
+        'pycsw:ConditionApplyingToAccessAndUse': 'conditionapplyingtoaccessanduse',
+        'pycsw:Lineage': 'lineage',
+        'pycsw:ResponsiblePartyRole': 'responsiblepartyrole',
+        'pycsw:SpecificationTitle': 'specificationtitle',
+        'pycsw:SpecificationDate': 'specificationdate',
+        'pycsw:SpecificationDateType': 'specificationdatetype',
+        'pycsw:Creator': 'creator',
+        'pycsw:Publisher': 'publisher',
+        'pycsw:Contributor': 'contributor',
+        'pycsw:Relation': 'relation',
+        'pycsw:Links': 'links',
+    }
+}
 
 
 def csw_view(request, catalog=None):
@@ -30,7 +136,12 @@ def csw_view(request, catalog=None):
     if request.method == 'POST':
         env['wsgi.input'] = StringIO(request.body)
 
-    csw = server.Csw(env)
+    # pycsw prefers absolute urls, let's get them from the request.
+    url = request.build_absolute_uri()
+    PYCSW['server']['url'] = url
+    PYCSW['metadata:main']['provider_url'] = url
+
+    csw = server.Csw(PYCSW, env)
     status, content = csw.dispatch_wsgi()
     status_code = int(status[0:3])
     response = HttpResponse(content,
@@ -45,6 +156,10 @@ urlpatterns = [
     url(r'^$', csw_view),
     url(r'^(?P<catalog>\w+)?$', csw_view),
 ]
+
+
+class RegistryRepository(Repository):
+    pass
 
 
 if __name__ == '__main__':  # pragma: no cover
